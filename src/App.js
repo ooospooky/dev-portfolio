8650dee3d28bf4f745e30037ea50b0f5155b0bec
Johnny Tai <bm414148@gmail.com>
2023-07-03 15:57:35 +0800

    basic projects done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Hero } from "./components/Hero/Hero";
+import { Projects } from "./components/Projects/Projects";
 import { Skills } from "./components/Skills/Skills";
 function App() {
   return (
     <div className="App">
         <Hero/>
         <Skills />
+        <Projects />
     </div>
   );
 }
