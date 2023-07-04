33a61df209492734ef77bfafe75ba8e20e98202e
Johnny Tai <bm414148@gmail.com>
2023-07-04 15:38:12 +0800

    basic skills done

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,7 +1,82 @@
 import React from 'react'
-
+import javascript from '../../images/skills/javascript.png'
+import typescript from '../../images/skills/typescript.png'
+import python from '../../images/skills/python.png'
+import html5 from '../../images/skills/html5.png'
+import css from '../../images/skills/css.png'
+import sass from '../../images/skills/sass.png'
+import reactlogo from '../../images/skills/react.png'
+import redux from '../../images/skills/redux.png'
+import firebase from '../../images/skills/firebase.png'
+import linux from '../../images/skills/linux3.png'
+import git from '../../images/skills/git.png'
 export const Skills = () => {
   return (
-    <section id="skills" className='skills'>Skills</section>
+    <section id="skills" className='skills'>
+      <h3>Skills</h3>
+      {/* Languages */}
+      <div className='skills__warper'>
+        <h4>Languages</h4>
+        <div className='skills__content'>
+          <div>
+            <img src={javascript} />
+            <p>Javascript</p>
+          </div>
+          <div>
+            <img src={typescript} />
+            <p>Typescript</p>
+          </div>
+          <div>
+            <img src={python} />
+            <p>Python</p>
+          </div>
+        </div>
+        </div>
+        {/* Front End */}
+        <div className='skills__warper'>
+          <h4>Front End</h4>
+          <div className='skills__content'>
+            <div>
+              <img src={reactlogo} />
+              <p>React</p>
+            </div>
+            <div>
+              <img src={sass} />
+              <p>Scss</p>
+            </div>
+            <div>
+              <img src={redux} />
+              <p>Redux</p>
+            </div>
+            <div>
+              <img src={html5} />
+              <p>HTML5</p>
+            </div>
+            <div>
+              <img src={css} />
+              <p>Css</p>
+            </div>
+          </div>
+        </div>
+        {/* Other */}
+        <div className='skills__warper'>
+          <h4>Others</h4>
+          <div className='skills__content'>
+            <div>
+              <img src={git} />
+              <p>Git</p>
+            </div>
+            <div>
+              <img src={linux} />
+              <p>Linux</p>
+            </div>
+            <div>
+              <img src={firebase} />
+              <p>Firebase</p>
+            </div>
+          </div>
+        </div>
+        
+    </section >
   )
 }
