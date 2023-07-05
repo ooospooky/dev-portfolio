1b263a800c59871c82cca678d20501fc7d106551
Johnny Tai <bm414148@gmail.com>
2023-07-05 15:53:43 +0800

    add RWD

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,13 +3,12 @@ import React from 'react'
 export const About = () => {
   return (
     <section id="about" className='about'>
+
       <div className='about__img'>
-        {/* <img src={selfPhoto} /> */}
       </div>
       <div className='about__content'>
         <h2 className='about__h2'>About Me</h2>
         <h3>A dedicated Front-end Developer</h3>
-
         <p>As a Junior Front-End Developer, I excel in designing and maintaining responsive websites that offer a smooth user experience. I have a deep understanding of HTML, CSS, and JavaScript and can create visually appealing web elements and effects using these technologies. I also familiar with the React framework and can develop highly interactive and scalable single-page applications. Additionally, I am proficient in CSS preprocessors like SCSS, which help me organize and manage code more efficiently, speeding up the development process.</p>
       </div>
     </section>
