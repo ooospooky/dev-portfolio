95504223087b9dd6b3667a85f8eb459e42b1b2a0
Johnny Tai <bm414148@gmail.com>
2023-07-05 16:44:15 +0800

    export customAnimation

---
+++ src/Animations/Animation.js
@@ -0,0 +1,11 @@
+import { keyframes } from "@emotion/react";
+export const customAnimation = keyframes`
+  from {
+    opacity: 0;
+    transform: translate3d(-100px, 0, 0);
+  }
+  to {
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
+  }
+`;
