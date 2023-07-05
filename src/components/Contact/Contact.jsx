c10fa64f330d36cd29185b2b483662ab390c7784
Johnny Tai <bm414148@gmail.com>
2023-07-05 13:39:11 +0800

    render feedback after submit

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,36 +1,44 @@
 import React, { useState } from 'react'
 import emailjs from 'emailjs-com';
-
+import { Player } from '@lottiefiles/react-lottie-player';
+import emailSent from '../../images/animations/emailSent.json'
 export const Contact = () => {
 
   const [name, setName] = useState('');
-  const [email,setEmail] = useState('')
+  const [email, setEmail] = useState('')
   const [content, setContent] = useState('');
-  const [haveSubmit,setHaveSubmit] = useState(true)
+  const [haveSubmit, setHaveSubmit] = useState(false)
   const onSubmit = (e) => {
     e.preventDefault();
     emailjs.send('service_d49m74o', 'template_kvdlecf', {
       name: name,
-      email:email,
+      email: email,
       content: content
-    }, 'aTBJM00DT9GDdbq16')  
+    }, 'aTBJM00DT9GDdbq16')
       .then((response) => {
         console.log('Email sent successfully!', response.status, response.text);
         setHaveSubmit(!haveSubmit);
         setName('');
         setEmail('');
         setContent('');
-        
+
       })
       .catch((error) => {
         console.error('Error sending email:', error);
       });
   };
-  if(haveSubmit){
+  if (haveSubmit) {
     return (
       <section className="haveSubmit" id="contact">
         <h2 className='haveSubmit__h2'>Thanks for reaching out!</h2>
-      <p className='haveSubmit__p'>Your form submission has been received.</p> 
+        <p className='haveSubmit__p'>Your form submission has been received.</p>
+        <Player
+          className='haveSubmit__emailAnimation'
+          autoplay
+          loop
+          src={emailSent}
+        >
+        </Player>
       </section>
     )
   }
@@ -51,9 +59,9 @@ export const Contact = () => {
         <input required type="email" name="email" className="input" placeholder="Email address" value={email} onChange={(e) => setEmail(e.target.value)} />
 
         <textarea required name="message" rows="6" className="input" placeholder="Type some message" value={content} onChange={(e) => setContent(e.target.value)} />
-      
 
-        <button  className="cta-btn cta-btn--hero contactForm__btn" type="submit">Submit</button>
+
+        <button className="cta-btn cta-btn--hero contactForm__btn" type="submit">Submit</button>
       </form>
     </section>
   )
