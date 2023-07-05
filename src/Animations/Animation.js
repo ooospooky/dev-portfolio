f77e3280883cceb6c837e26014d32f1e477ab9b3
Johnny Tai <bm414148@gmail.com>
2023-07-05 16:56:14 +0800

    add left200Animation

--- src/Animations/Animation.js
+++ src/Animations/Animation.js
@@ -1,8 +1,21 @@
 import { keyframes } from "@emotion/react";
 export const customAnimation = keyframes`
+  triggerOnce={true}
   from {
     opacity: 0;
     transform: translate3d(-100px, 0, 0);
+  }
+  to {
+    opacity: 1;
+    transform: translate3d(0, 0, 0);
+  }
+`;
+
+export const left200Animation = keyframes`
+  triggerOnce={true}
+  from {
+    opacity: 0;
+    transform: translate3d(-200px, 0, 0);
   }
   to {
     opacity: 1;
