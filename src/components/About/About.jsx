c09a54e17701e7813ffbd47514e6ef466be22455
Johnny Tai <bm414148@gmail.com>
2023-07-05 14:04:04 +0800

    add h2

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,6 +7,7 @@ export const About = () => {
         {/* <img src={selfPhoto} /> */}
       </div>
       <div className='about__content'>
+        <h2 className='about__h2'>About Me</h2>
         <h3>A dedicated Front-end Developer</h3>
 
         <p>As a Junior Front-End Developer, I excel in designing and maintaining responsive websites that offer a smooth user experience. I have a deep understanding of HTML, CSS, and JavaScript and can create visually appealing web elements and effects using these technologies. I also familiar with the React framework and can develop highly interactive and scalable single-page applications. Additionally, I am proficient in CSS preprocessors like SCSS, which help me organize and manage code more efficiently, speeding up the development process.</p>
