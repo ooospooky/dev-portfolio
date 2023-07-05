080df4f0e0d6ec8cbfde2d03b044fe437624fe8c
Johnny Tai <bm414148@gmail.com>
2023-07-05 16:57:26 +0800

    add left fade animation

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { Fade } from "react-awesome-reveal";
+import { left200Animation } from '../../Animations/Animation'
+
 import javascript from '../../images/skills/javascript.png'
 import typescript from '../../images/skills/typescript.png'
 import python from '../../images/skills/python.png'
@@ -11,28 +14,32 @@ import firebase from '../../images/skills/firebase.png'
 import linux from '../../images/skills/linux3.png'
 import git from '../../images/skills/git.png'
 export const Skills = () => {
+
   return (
     <section id="skills" className='skills'>
       <h3>Skills</h3>
       {/* Languages */}
-      <div className='skills__warper'>
-        <h4>Languages</h4>
-        <div className='skills__content'>
-          <div>
-            <img src={javascript} />
-            <p>Javascript</p>
-          </div>
-          <div>
-            <img src={typescript} />
-            <p>Typescript</p>
-          </div>
-          <div>
-            <img src={python} />
-            <p>Python</p>
+      <Fade triggerOnce={true} delay={300} keyframes={left200Animation} >
+        <div className='skills__warper'>
+          <h4>Languages</h4>
+          <div className='skills__content'>
+            <div>
+              <img src={javascript} />
+              <p>Javascript</p>
+            </div>
+            <div>
+              <img src={typescript} />
+              <p>Typescript</p>
+            </div>
+            <div>
+              <img src={python} />
+              <p>Python</p>
+            </div>
           </div>
         </div>
-        </div>
-        {/* Front End */}
+      </Fade>
+      {/* Front End */}
+      <Fade triggerOnce={true} delay={500} keyframes={left200Animation} >
         <div className='skills__warper'>
           <h4>Front End</h4>
           <div className='skills__content'>
@@ -58,6 +65,8 @@ export const Skills = () => {
             </div>
           </div>
         </div>
+      </Fade>
+      <Fade triggerOnce={true} delay={700} keyframes={left200Animation} >
         {/* Other */}
         <div className='skills__warper'>
           <h4>Others</h4>
@@ -76,7 +85,7 @@ export const Skills = () => {
             </div>
           </div>
         </div>
-        
+      </Fade>
     </section >
   )
 }
