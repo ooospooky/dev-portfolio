1e4de887579b2f54ad237d8a93df4fd2bce5be77
Johnny Tai <bm414148@gmail.com>
2023-07-18 16:07:34 +0800

    add fontend skill Next.js and remove css

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,28 +1,28 @@
-import React from 'react'
+import React from "react";
 import { Fade } from "react-awesome-reveal";
-import { left200Animation } from '../../Animations/Animation'
+import { left200Animation } from "../../Animations/Animation";
 
-import javascript from '../../images/skills/javascript.png'
-import typescript from '../../images/skills/typescript.png'
-import python from '../../images/skills/python.png'
-import html5 from '../../images/skills/html5.png'
-import css from '../../images/skills/css.png'
-import sass from '../../images/skills/sass.png'
-import reactlogo from '../../images/skills/react.png'
-import redux from '../../images/skills/redux.png'
-import firebase from '../../images/skills/firebase.png'
-import linux from '../../images/skills/linux3.png'
-import git from '../../images/skills/git.png'
+import javascript from "../../images/skills/javascript.png";
+import typescript from "../../images/skills/typescript.png";
+import python from "../../images/skills/python.png";
+import html5 from "../../images/skills/html5.png";
+import css from "../../images/skills/css.png";
+import sass from "../../images/skills/sass.png";
+import reactlogo from "../../images/skills/react.png";
+import redux from "../../images/skills/redux.png";
+import firebase from "../../images/skills/firebase.png";
+import linux from "../../images/skills/linux3.png";
+import git from "../../images/skills/git.png";
+import nextjs from "../../images/skills/nextjs.png";
 export const Skills = () => {
-
   return (
-    <section id="skills" className='skills'>
+    <section id="skills" className="skills">
       <h3>Skills</h3>
       {/* Languages */}
-      <Fade triggerOnce={true} delay={300} keyframes={left200Animation} >
-        <div className='skills__warper'>
+      <Fade triggerOnce={true} delay={300} keyframes={left200Animation}>
+        <div className="skills__warper">
           <h4>Languages</h4>
-          <div className='skills__content'>
+          <div className="skills__content">
             <div>
               <img src={javascript} />
               <p>Javascript</p>
@@ -39,10 +39,14 @@ export const Skills = () => {
         </div>
       </Fade>
       {/* Front End */}
-      <Fade triggerOnce={true} delay={500} keyframes={left200Animation} >
-        <div className='skills__warper'>
+      <Fade triggerOnce={true} delay={500} keyframes={left200Animation}>
+        <div className="skills__warper">
           <h4>Front End</h4>
-          <div className='skills__content'>
+          <div className="skills__content">
+            <div>
+              <img src={nextjs} />
+              <p>Next.js</p>
+            </div>
             <div>
               <img src={reactlogo} />
               <p>React</p>
@@ -59,18 +63,14 @@ export const Skills = () => {
               <img src={html5} />
               <p>HTML5</p>
             </div>
-            <div>
-              <img src={css} />
-              <p>Css</p>
-            </div>
           </div>
         </div>
       </Fade>
-      <Fade triggerOnce={true} delay={700} keyframes={left200Animation} >
+      <Fade triggerOnce={true} delay={700} keyframes={left200Animation}>
         {/* Other */}
-        <div className='skills__warper'>
+        <div className="skills__warper">
           <h4>Others</h4>
-          <div className='skills__content'>
+          <div className="skills__content">
             <div>
               <img src={git} />
               <p>Git</p>
@@ -86,6 +86,6 @@ export const Skills = () => {
           </div>
         </div>
       </Fade>
-    </section >
-  )
-}
+    </section>
+  );
+};
