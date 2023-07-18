2b087a780882e44bd306d7067f2d32c86b7e9c4f
Johnny Tai <bm414148@gmail.com>
2023-07-18 16:15:14 +0800

    change dashboard to food order system

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,48 +1,65 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { Fade } from "react-awesome-reveal";
 
-import { fadeupAnimation } from '../../Animations/Animation'
-import chart from '../../images/chart.png'
-import whatShouldIWear from '../../images/whatShouldIWear.png';
+import { fadeupAnimation } from "../../Animations/Animation";
+import chart from "../../images/chart.png";
+import whatShouldIWear from "../../images/whatShouldIWear.png";
+import foodOrder from "../../images/foodOrder.png";
 export const Projects = () => {
   return (
-    <section id="project" className='project'>
-      <div className='container'>
-        <h2 className='project__h2'> PROJECTS </h2>
-        <Fade triggerOnce={true} delay={300} keyframes={fadeupAnimation} >
-
-          <div className='project-wrapper project-wrapper__odd'>
-            <div className='projectImg'>
-              <a href="https://github.com/ooospooky/What-Should-I-Wear" target="_blank" >
+    <section id="project" className="project">
+      <div className="container">
+        <h2 className="project__h2"> PROJECTS </h2>
+        <Fade triggerOnce={true} delay={300} keyframes={fadeupAnimation}>
+          <div className="project-wrapper project-wrapper__odd">
+            <div className="projectImg">
+              <a
+                href="https://github.com/ooospooky/What-Should-I-Wear"
+                target="_blank"
+                rel="noreferrer"
+              >
                 {/* <img className='img' src="https://i.ibb.co/5s6PrR8/2023-06-07-3-04-16.png" /> */}
 
-                <img className='img' src={whatShouldIWear} />
+                <img className="img" src={whatShouldIWear} />
               </a>
-
             </div>
-            <div className='projectContent'>
-              <h3 className='projectContent__h3'>What Should I Wear</h3>
-              <p className='projectContent__p'> Make your life easier and more comfortable when it comes to deciding what to wear. By leveraging weather data, our service offers dress recommendations based on the current and forecasted weather conditions. </p>
-              <div className='projectContent__stack'>
+            <div className="projectContent">
+              <h3 className="projectContent__h3">What Should I Wear</h3>
+              <p className="projectContent__p">
+                {" "}
+                Make your life easier and more comfortable when it comes to
+                deciding what to wear. By leveraging weather data, our service
+                offers dress recommendations based on the current and forecasted
+                weather conditions.{" "}
+              </p>
+              <div className="projectContent__stack">
                 <p>React</p>
                 <p>Scss</p>
               </div>
-              <div className='projectContent__btn'>
+              <div className="projectContent__btn">
                 <p className="project__cta">
-                  <a href="https://github.com/ooospooky/What-Should-I-Wear" target="_blank" >
+                  <a
+                    href="https://github.com/ooospooky/What-Should-I-Wear"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
                     <span className="cta-btn cta-btn--hero">
-                      Code	&thinsp;
+                      Code &thinsp;
                       <FontAwesomeIcon icon={faGithub} />
                     </span>
                   </a>
                 </p>
                 <p className="project__cta">
-                  <a href="https://what-should-i-wear-bd38a.web.app/" target="_blank" >
+                  <a
+                    href="https://what-should-i-wear-bd38a.web.app/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
                     <span className="cta-btn cta-btn--hero">
-                      Live Demo	&thinsp;
+                      Live Demo &thinsp;
                       {/* <FontAwesomeIcon icon={faArrowAltUpRight} /> */}
                       <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
                     </span>
@@ -52,31 +69,41 @@ export const Projects = () => {
             </div>
           </div>
         </Fade>
-        {/* Dashboard */}
-        <Fade triggerOnce={true} delay={300} keyframes={fadeupAnimation} >
-          <div className='project-wrapper project-wrapper__even'>
-            <div className='projectContent'>
-              <h3 className='projectContent__h3'>Dashboard</h3>
-              <p className='projectContent__p'>
-              Chart of Household and Population by Household Type and Gender. Presenting charts using Highcharts.</p>
-              <div className='projectContent__stack'>
-                <p>React</p>
+        {/* Food Order System */}
+        <Fade triggerOnce={true} delay={300} keyframes={fadeupAnimation}>
+          <div className="project-wrapper project-wrapper__even">
+            <div className="projectContent">
+              <h3 className="projectContent__h3">Food Order System</h3>
+              <p className="projectContent__p">
+                Allows users to browse food places, add items to the cart, and
+                submit orders.
+              </p>
+              <div className="projectContent__stack">
+                <p>Next.js</p>
                 <p>Typescript</p>
-                <p>Scss</p>
+                <p>Redux</p>
               </div>
-              <div className='projectContent__btn'>
+              <div className="projectContent__btn">
                 <p className="project__cta">
-                  <a href="https://github.com/ooospooky/dashboard" target="_blank" >
+                  <a
+                    href="https://github.com/ooospooky/food-order-system"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
                     <span className="cta-btn cta-btn--hero">
-                      Code	&thinsp;
+                      Code &thinsp;
                       <FontAwesomeIcon icon={faGithub} />
                     </span>
                   </a>
                 </p>
                 <p className="project__cta">
-                  <a href="https://dashboard-3daad.web.app/" target="_blank" >
+                  <a
+                    href="https://food-order-system-flax.vercel.app/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
                     <span className="cta-btn cta-btn--hero">
-                      Live Demo	&thinsp;
+                      Live Demo &thinsp;
                       {/* <FontAwesomeIcon icon={faArrowAltUpRight} /> */}
                       <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
                     </span>
@@ -84,42 +111,66 @@ export const Projects = () => {
                 </p>
               </div>
             </div>
-            <div className='projectImg'>
-              <a href="https://dashboard-3daad.web.app/" target="_blank" >
-                <img className='img' src={chart} />
+            <div className="projectImg">
+              <a
+                href="https://food-order-system-flax.vercel.app/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img className="img" src={foodOrder} />
               </a>
             </div>
           </div>
         </Fade>
         {/* Cool Camp */}
-        <Fade triggerOnce={true} delay={300} keyframes={fadeupAnimation} >
-          <div className='project-wrapper project-wrapper__odd'>
-          <div className='projectImg'>
-              <a href="https://coolcamp-f0b6e.web.app/" target="_blank" >
-                <img className='img' src="https://i.ibb.co/RYVhTDC/2023-07-03-2-59-57.png" />
+        <Fade triggerOnce={true} delay={300} keyframes={fadeupAnimation}>
+          <div className="project-wrapper project-wrapper__odd">
+            <div className="projectImg">
+              <a
+                href="https://coolcamp-f0b6e.web.app/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  className="img"
+                  src="https://i.ibb.co/RYVhTDC/2023-07-03-2-59-57.png"
+                />
               </a>
             </div>
-            <div className='projectContent'>
-              <h3 className='projectContent__h3'>Cool Camp</h3>
-              <p className='projectContent__p'>
-                Let users to choose a camping site that suits their needs and preferences. Users can browse through various camping sites and make decisions based on ratings and reviews from other users. Additionally, users can provide their own ratings and leave feedback, sharing their thoughts and suggestions. </p>
-              <div className='projectContent__stack'>
+            <div className="projectContent">
+              <h3 className="projectContent__h3">Cool Camp</h3>
+              <p className="projectContent__p">
+                Let users to choose a camping site that suits their needs and
+                preferences. Users can browse through various camping sites and
+                make decisions based on ratings and reviews from other users.
+                Additionally, users can provide their own ratings and leave
+                feedback, sharing their thoughts and suggestions.{" "}
+              </p>
+              <div className="projectContent__stack">
                 <p>React</p>
                 <p>Bootstarp</p>
               </div>
-              <div className='projectContent__btn'>
+              <div className="projectContent__btn">
                 <p className="project__cta">
-                  <a href="https://github.com/ooospooky/CoolCamp" target="_blank" >
+                  <a
+                    href="https://github.com/ooospooky/CoolCamp"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
                     <span className="cta-btn cta-btn--hero">
-                      Code	&thinsp;
+                      Code &thinsp;
                       <FontAwesomeIcon icon={faGithub} />
                     </span>
                   </a>
                 </p>
                 <p className="project__cta">
-                  <a href="https://coolcamp-f0b6e.web.app/" target="_blank" >
+                  <a
+                    href="https://coolcamp-f0b6e.web.app/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
                     <span className="cta-btn cta-btn--hero">
-                      Live Demo	&thinsp;
+                      Live Demo &thinsp;
                       {/* <FontAwesomeIcon icon={faArrowAltUpRight} /> */}
                       <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
                     </span>
@@ -127,11 +178,9 @@ export const Projects = () => {
                 </p>
               </div>
             </div>
-      
           </div>
         </Fade>
-
       </div>
-    </section >
-  )
-}
+    </section>
+  );
+};
