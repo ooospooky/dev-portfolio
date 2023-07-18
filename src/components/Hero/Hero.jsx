e3181e23dfce9f841f2e6cc3576b543f96560c2a
Johnny Tai <bm414148@gmail.com>
2023-07-18 16:02:20 +0800

    update resume URL

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,40 +1,45 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFile } from '@fortawesome/free-solid-svg-icons';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFile } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { Fade } from "react-awesome-reveal";
 
-import {customAnimation} from '../../Animations/Animation'
+import { customAnimation } from "../../Animations/Animation";
 
 export const Hero = () => {
-  
   return (
-    <section id="hero" className='hero' >
-      <Fade  triggerOnce={true}  delay={500} keyframes={customAnimation} >
-      <h1 className="hero__h1">
-        Hi, I'm <span className="hero__colorH1">Johnny.</span>
-        <br />
-        I'm a Front End Developer.
-      </h1>
-      </Fade >
-      <Fade  triggerOnce={true} delay={1000} keyframes={customAnimation} >
-      <p className="hero__cta">
-        <a href="https://github.com/ooospooky" target="_blank" >
-          <span className="cta-btn cta-btn--hero">
-            GitHub	&thinsp;
-            <FontAwesomeIcon icon={faGithub} />
-          </span>
-        </a>
-        <a href="https://drive.google.com/file/d/1jkxZWmulYdhsZ8E2KvJIkOTPG2zbce-e/view?usp=sharing" target="_blank" >
-          <span className="cta-btn cta-btn--hero">
-            Resume  &thinsp;
-            <FontAwesomeIcon icon={faFile} />
-          </span>
-        </a>
-      </p>
+    <section id="hero" className="hero">
+      <Fade triggerOnce={true} delay={500} keyframes={customAnimation}>
+        <h1 className="hero__h1">
+          Hi, I'm <span className="hero__colorH1">Johnny.</span>
+          <br />
+          I'm a Front End Developer.
+        </h1>
       </Fade>
-
-
+      <Fade triggerOnce={true} delay={1000} keyframes={customAnimation}>
+        <p className="hero__cta">
+          <a
+            href="https://github.com/ooospooky"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span className="cta-btn cta-btn--hero">
+              GitHub &thinsp;
+              <FontAwesomeIcon icon={faGithub} />
+            </span>
+          </a>
+          <a
+            href="https://drive.google.com/file/d/1n_iu6ktPa7hCw80UvbZ09AzogynCQhee/view?usp=sharing"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <span className="cta-btn cta-btn--hero">
+              Resume &thinsp;
+              <FontAwesomeIcon icon={faFile} />
+            </span>
+          </a>
+        </p>
+      </Fade>
     </section>
-  )
-}
+  );
+};
