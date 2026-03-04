1dbe514746e9ce7a9c55a2cbba63b45b541ffcf8
ooospooky <bm414148@gmail.com>
2026-03-04 16:30:56 +0800

    chore: update resume link

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -40,7 +40,7 @@ export const Hero = () => {
             </span>
           </a>
           <a
-            href="https://drive.google.com/file/d/1P5t-hTSzaKTubO6293PEVyblJLkbb6QL/view?usp=sharing"
+            href="https://drive.google.com/file/d/1VsS9pqiMZG-D2NN-zmSc1Fp7ztUjW-lv/view?usp=sharing"
             target="_blank"
             rel="noreferrer"
           >
